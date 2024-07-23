68cc929601db91d0d58014feb6b698bd5fc73d5b
Sammy Spets <1011469+sammys@users.noreply.github.com>
2024-07-23 13:11:53 +0700

    Add location overrides to CatalogModifier Support CatalogItem::categories [since 2023-12-13]

---
+++ src/models/catalog_item_category.rs
@@ -0,0 +1,12 @@
+//! Model struct for CatalogV1Id type.
+
+use serde::{Deserialize, Serialize};
+
+/// A Square API V1 identifier of an item, including the object ID and its associated location ID.
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+pub struct CatalogItemCategory {
+    /// The ID of the item's category, if any.
+    pub id: Option<String>,
+    /// The ID of the `Location` this Connect V1 ID is associated with.
+    pub ordinal: Option<i64>,
+}
---
+++ src/models/modifier_location_overrides.rs
@@ -0,0 +1,15 @@
+//! Model struct for ModifierLocationOverrides type.
+
+use serde::{Deserialize, Serialize};
+
+use super::Money;
+
+/// Price and inventory alerting overrides for a `CatalogItemVariation` at a specific `Location`.
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+pub struct ModifierLocationOverrides {
+    /// The ID of the `Location`. This can include locations that are deactivated.
+    pub location_id: Option<String>,
+    /// The price of the `CatalogItemVariation` at the given `Location`, or blank for variable
+    /// pricing.
+    pub price_money: Option<Money>,
+}
